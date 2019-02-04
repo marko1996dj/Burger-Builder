488d3e52631fdc362b0c9d167478ce99120bf86d
marko1996dj <marko96djuric@gmail.com>
2019-02-04 14:51:54 +0100

    Updates

---
+++ precache-manifest.753042f9d0605d2d3e85934835ef90dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6d044836b4998e16b97a",
+    "url": "/static/js/main.6d044836.chunk.js"
+  },
+  {
+    "revision": "f453b9c758644fc96b26",
+    "url": "/static/js/1.f453b9c7.chunk.js"
+  },
+  {
+    "revision": "6d044836b4998e16b97a",
+    "url": "/static/css/main.c36249e4.chunk.css"
+  },
+  {
+    "revision": "262cfab4c4b4bb7eaba256e43ed56d7d",
+    "url": "/index.html"
+  }
+];
